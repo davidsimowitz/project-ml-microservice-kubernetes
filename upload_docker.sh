d802d90e597573aaf26c0fc2f7280384f4c5f756
David Simowitz <david.simowitz@gmail.com>
2020-05-13 15:10:05 -0400

    style: clean up comments

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
-# This file tags and uploads an image to Docker Hub
 
+# This file tags and uploads an image to Docker Hub
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
 # Create dockerpath
 dockerpath=ml-microservice-api
 
-# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 docker login --username davidsimowitz
 docker tag ml-microservice-api davidsimowitz/ml-microservice-api:1.1
 
-# Step 3:
 # Push image to a docker repository
 docker push davidsimowitz/ml-microservice-api:1.1
